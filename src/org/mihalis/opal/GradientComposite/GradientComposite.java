9178ce284307892520d5454350807f4cb9c78a09
laurent caron <laurent.caron@gmail.com>
2011-05-01 12:46:21 +0200

    Improve & fix comments

--- src/org/mihalis/opal/GradientComposite/GradientComposite.java
+++ src/org/mihalis/opal/GradientComposite/GradientComposite.java
@@ -27,128 +27,138 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Widget;
 
 /**
- * Instances of this class are controls which background's texture is brushed metal "à la Mac"
+ * Instances of this class are controls which background's texture is a gradient
+ * composite
  */
 public class GradientComposite extends Composite {
-    private Image oldImage;
-    private Color gradientEnd;
-    private Color gradientStart;
+	private Image oldImage;
+	private Color gradientEnd;
+	private Color gradientStart;
 
-    /**
-     * Constructs a new instance of this class given its parent and a style value describing its behavior and appearance.
-     * <p>
-     * The style value is either one of the style constants defined in class <code>SWT</code> which is applicable to instances of this class, or must be built by <em>bitwise OR</em>'ing together (that is, using the <code>int</code> "|" operator) two or more of those <code>SWT</code> style constants. The class description lists the style constants that are applicable to the class. Style bits are also inherited from superclasses.
-     * </p>
-     * 
-     * @param parent a widget which will be the parent of the new instance (cannot be null)
-     * @param style the style of widget to construct
-     * 
-     * @exception IllegalArgumentException <ul>
-     *                <li>ERROR_NULL_ARGUMENT - if the parent is null</li>
-     *                </ul>
-     * @exception SWTException <ul>
-     *                <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the parent</li>
-     *                </ul>
-     * 
-     * @see Composite#Composite(Composite, int)
-     * @see SWT#NO_BACKGROUND
-     * @see SWT#NO_FOCUS
-     * @see SWT#NO_MERGE_PAINTS
-     * @see SWT#NO_REDRAW_RESIZE
-     * @see SWT#NO_RADIO_GROUP
-     * @see SWT#EMBEDDED
-     * @see SWT#DOUBLE_BUFFERED
-     * @see Widget#getStyle
-     */
-    public GradientComposite(final Composite parent, final int style) {
-        super(parent, style);
-        this.addListener(SWT.Resize, new Listener() {
-            public void handleEvent(final Event event) {
-                GradientComposite.this.redrawComposite();
-            }
-        });
+	/**
+	 * Constructs a new instance of this class given its parent and a style
+	 * value describing its behavior and appearance.
+	 * <p>
+	 * The style value is either one of the style constants defined in class
+	 * <code>SWT</code> which is applicable to instances of this class, or must
+	 * be built by <em>bitwise OR</em>'ing together (that is, using the
+	 * <code>int</code> "|" operator) two or more of those <code>SWT</code>
+	 * style constants. The class description lists the style constants that are
+	 * applicable to the class. Style bits are also inherited from superclasses.
+	 * </p>
+	 * 
+	 * @param parent a widget which will be the parent of the new instance
+	 *            (cannot be null)
+	 * @param style the style of widget to construct
+	 * 
+	 * @exception IllegalArgumentException <ul>
+	 *                <li>ERROR_NULL_ARGUMENT - if the parent is null</li>
+	 *                </ul>
+	 * @exception SWTException <ul>
+	 *                <li>ERROR_THREAD_INVALID_ACCESS - if not called from the
+	 *                thread that created the parent</li>
+	 *                </ul>
+	 * 
+	 * @see Composite#Composite(Composite, int)
+	 * @see SWT#NO_BACKGROUND
+	 * @see SWT#NO_FOCUS
+	 * @see SWT#NO_MERGE_PAINTS
+	 * @see SWT#NO_REDRAW_RESIZE
+	 * @see SWT#NO_RADIO_GROUP
+	 * @see SWT#EMBEDDED
+	 * @see SWT#DOUBLE_BUFFERED
+	 * @see Widget#getStyle
+	 */
+	public GradientComposite(final Composite parent, final int style) {
+		super(parent, style);
+		this.addListener(SWT.Resize, new Listener() {
+			@Override
+			public void handleEvent(final Event event) {
+				GradientComposite.this.redrawComposite();
+			}
+		});
 
-        parent.addDisposeListener(new DisposeListener() {
+		parent.addDisposeListener(new DisposeListener() {
 
-            @Override
-            public void widgetDisposed(final DisposeEvent e) {
-                this.dispose(GradientComposite.this.oldImage);
-                this.dispose(GradientComposite.this.gradientEnd);
-                this.dispose(GradientComposite.this.gradientStart);
-            }
+			@Override
+			public void widgetDisposed(final DisposeEvent e) {
+				this.dispose(GradientComposite.this.oldImage);
+				this.dispose(GradientComposite.this.gradientEnd);
+				this.dispose(GradientComposite.this.gradientStart);
+			}
 
-            private void dispose(final Resource resource) {
-                if (resource != null && !resource.isDisposed()) {
-                    resource.dispose();
-                }
-            }
-        });
+			private void dispose(final Resource resource) {
+				if (resource != null && !resource.isDisposed()) {
+					resource.dispose();
+				}
+			}
+		});
 
-        this.gradientEnd = new Color(this.getDisplay(), 110, 110, 110);
-        this.gradientStart = new Color(this.getDisplay(), 0, 0, 0);
+		this.gradientEnd = new Color(this.getDisplay(), 110, 110, 110);
+		this.gradientStart = new Color(this.getDisplay(), 0, 0, 0);
 
-    }
+	}
 
-    /**
-     * Redraws the composite
-     */
-    private void redrawComposite() {
-        final Display display = this.getDisplay();
-        final Rectangle rect = this.getClientArea();
-        final Image newImage = new Image(display, Math.max(1, rect.width), Math.max(1, rect.height));
+	/**
+	 * Redraws the composite
+	 */
+	private void redrawComposite() {
+		final Display display = this.getDisplay();
+		final Rectangle rect = this.getClientArea();
+		final Image newImage = new Image(display, Math.max(1, rect.width), Math.max(1, rect.height));
 
-        final GC gc = new GC(newImage);
-        gc.setForeground(this.gradientStart);
-        gc.setBackground(this.gradientEnd);
+		final GC gc = new GC(newImage);
+		gc.setForeground(this.gradientStart);
+		gc.setBackground(this.gradientEnd);
 
-        gc.fillGradientRectangle(rect.x, rect.y, rect.width, rect.height / 2, true);
+		gc.fillGradientRectangle(rect.x, rect.y, rect.width, rect.height / 2, true);
 
-        gc.setForeground(this.gradientEnd);
-        gc.setBackground(this.gradientStart);
+		gc.setForeground(this.gradientEnd);
+		gc.setBackground(this.gradientStart);
 
-        gc.fillGradientRectangle(rect.x, rect.height / 2, rect.width, rect.height / 2, true);
-        gc.dispose();
+		gc.fillGradientRectangle(rect.x, rect.height / 2, rect.width, rect.height / 2, true);
+		gc.dispose();
 
-        this.setBackgroundImage(newImage);
-        if (this.oldImage != null) {
-            this.oldImage.dispose();
-        }
-        this.oldImage = newImage;
+		this.setBackgroundImage(newImage);
+		if (this.oldImage != null) {
+			this.oldImage.dispose();
+		}
+		this.oldImage = newImage;
 
-    }
+	}
 
-    // ------------------------------------ Getters and Setters
-    /**
-     * @return the gradientEnd color
-     */
-    public Color getGradientEnd() {
-        return this.gradientEnd;
-    }
+	// ------------------------------------ Getters and Setters
+	/**
+	 * @return the gradientEnd color
+	 */
+	public Color getGradientEnd() {
+		return this.gradientEnd;
+	}
 
-    /**
-     * @param gradientEnd the gradientEnd color to set
-     */
-    public void setGradientEnd(final Color gradientEnd) {
-        if (this.gradientEnd != null && !this.gradientEnd.isDisposed()) {
-            this.gradientEnd.dispose();
-        }
-        this.gradientEnd = gradientEnd;
-    }
+	/**
+	 * @param gradientEnd the gradientEnd color to set
+	 */
+	public void setGradientEnd(final Color gradientEnd) {
+		if (this.gradientEnd != null && !this.gradientEnd.isDisposed()) {
+			this.gradientEnd.dispose();
+		}
+		this.gradientEnd = gradientEnd;
+	}
 
-    /**
-     * @return the gradientStart color
-     */
-    public Color getGradientStart() {
-        return this.gradientStart;
-    }
+	/**
+	 * @return the gradientStart color
+	 */
+	public Color getGradientStart() {
+		return this.gradientStart;
+	}
 
-    /**
-     * @param gradientStart the gradientStart color to set
-     */
-    public void setGradientStart(final Color gradientStart) {
-        if (this.gradientStart != null && !this.gradientStart.isDisposed()) {
-            this.gradientStart.dispose();
-        }
-        this.gradientStart = gradientStart;
-    }
+	/**
+	 * @param gradientStart the gradientStart color to set
+	 */
+	public void setGradientStart(final Color gradientStart) {
+		if (this.gradientStart != null && !this.gradientStart.isDisposed()) {
+			this.gradientStart.dispose();
+		}
+		this.gradientStart = gradientStart;
+	}
 }

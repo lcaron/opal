1300b9025b1e792f10c1d1ff7a2ac75f8c59d718
laurent.caron <laurent.caron@localhost>
2012-08-27 22:24:16 +0200

    Initial Working Copy

--- src/main/java/org/mihalis/opal/obutton/ButtonRenderer.java
+++ src/main/java/org/mihalis/opal/obutton/ButtonRenderer.java
@@ -1,22 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Laurent CARON
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Laurent CARON (laurent.caron at gmail dot com) - initial API and implementation
+ *******************************************************************************/
 package org.mihalis.opal.obutton;
 
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 
+/**
+ * This interface contains methods used to render a button
+ */
 public interface ButtonRenderer {
 
+	/**
+	 * Dispose the elements when the button is disposed
+	 */
 	void dispose();
 
-	void drawButtonWhenMouseHover(GC gc);
+	/**
+	 * Draw the button when the mouse is hover
+	 * @param gc Graphical context
+	 * @param button button displayed
+	 */
+	void drawButtonWhenMouseHover(GC gc, OButton button);
 
-	void drawButtonWhenDisabled(GC gc);
+	/**
+	 * Draw the button when the button is disabled
+	 * @param gc Graphical context
+	 * @param button button displayed
+	 */
+	void drawButtonWhenDisabled(GC gc, OButton button);
 
-	void drawButtonWhenSelected(GC gc);
+	/**
+	 * Draw the button when the button (toggle button) is selected
+	 * @param gc Graphical context
+	 * @param button button displayed
+	 */
+	void drawButtonWhenSelected(GC gc, OButton button);
 
-	void drawButton(GC gc);
+	/**
+	 * @param gc
+	 * @param button
+	 */
+	void drawButton(GC gc, OButton button);
 
-	Point computeSize(int wHint, int hHint, boolean changed);
+	/**
+	 * Draw the button when the button is clicked
+	 * @param gc Graphical context
+	 * @param button button displayed
+	 */
+	void drawButtonWhenClicked(GC gc, OButton parent);
 
-	void drawButtonWhenClicked(GC gc);
-
+	/**
+	 * Compute the size of the button
+	 * @param button button associated to this renderer
+	 * @param wHint the width hint (can be <code>SWT.DEFAULT</code>)
+	 * @param hHint the height hint (can be <code>SWT.DEFAULT</code>)
+	 * @param changed <code>true</code> if the control's contents have changed, and <code>false</code> otherwise
+	 * @return the size of the button
+	 */
+	Point computeSize(OButton button, int wHint, int hHint, boolean changed);
 }

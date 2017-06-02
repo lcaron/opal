6be93cb4c1786f4c0eebf358a97c3eaced4ff9e7
Laurent CARON <laurent.caron@gmail.com>
2017-06-02 22:31:26 +0200

    Fix the copyright comments

--- src/main/java/org/mihalis/opal/calculator/Calculator.java
+++ src/main/java/org/mihalis/opal/calculator/Calculator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Laurent CARON
+ * Copyright (c) 2012-2017 Laurent CARON
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,7 +26,8 @@ import org.eclipse.swt.widgets.Label;
  * <dt><b>Events:</b></dt>
  * <dd>(none)</dd>
  * </dl>
- * This component is inspired by Hermant (http://www.javabeginner.com/java-swing/java-swing-calculator)
+ * This component is inspired by Hermant
+ * (http://www.javabeginner.com/java-swing/java-swing-calculator)
  */
 public class Calculator extends Composite {
 
@@ -34,38 +35,40 @@ public class Calculator extends Composite {
 	private final CalculatorButtonsComposite panel;
 
 	/**
-	 * Constructs a new instance of this class given its parent and a style
-	 * value describing its behavior and appearance.
+	 * Constructs a new instance of this class given its parent and a style value
+	 * describing its behavior and appearance.
 	 * <p>
 	 * The style value is either one of the style constants defined in class
-	 * <code>SWT</code> which is applicable to instances of this class, or must
-	 * be built by <em>bitwise OR</em>'ing together (that is, using the
-	 * <code>int</code> "|" operator) two or more of those <code>SWT</code>
-	 * style constants. The class description lists the style constants that are
+	 * <code>SWT</code> which is applicable to instances of this class, or must be
+	 * built by <em>bitwise OR</em>'ing together (that is, using the
+	 * <code>int</code> "|" operator) two or more of those <code>SWT</code> style
+	 * constants. The class description lists the style constants that are
 	 * applicable to the class. Style bits are also inherited from superclasses.
 	 * </p>
-	 * 
+	 *
 	 * @param parent a composite control which will be the parent of the new
 	 *            instance (cannot be null)
 	 * @param style the style of control to construct
-	 * 
-	 * @exception IllegalArgumentException <ul>
+	 *
+	 * @exception IllegalArgumentException
+	 *                <ul>
 	 *                <li>ERROR_NULL_ARGUMENT - if the parent is null</li>
 	 *                </ul>
-	 * @exception SWTException <ul>
+	 * @exception SWTException
+	 *                <ul>
 	 *                <li>ERROR_THREAD_INVALID_ACCESS - if not called from the
 	 *                thread that created the parent</li>
 	 *                </ul>
-	 * 
+	 *
 	 */
 	public Calculator(final Composite parent, final int style) {
 		super(parent, style);
 		setLayout(new GridLayout());
-		this.displayArea = createTextArea();
-		this.panel = new CalculatorButtonsComposite(this, SWT.NONE);
-		this.panel.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
-		this.panel.setDisplayArea(this.displayArea);
-		this.displayArea.addKeyListener(this.panel.getKeyListener());
+		displayArea = createTextArea();
+		panel = new CalculatorButtonsComposite(this, SWT.NONE);
+		panel.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
+		panel.setDisplayArea(displayArea);
+		displayArea.addKeyListener(panel.getKeyListener());
 	}
 
 	/**
@@ -82,28 +85,30 @@ public class Calculator extends Composite {
 	}
 
 	/**
-	 * Adds the listener to the collection of listeners who will be notified
-	 * when the receiver's text is modified, by sending it one of the messages
-	 * defined in the <code>ModifyListener</code> interface.
-	 * 
+	 * Adds the listener to the collection of listeners who will be notified when
+	 * the receiver's text is modified, by sending it one of the messages defined in
+	 * the <code>ModifyListener</code> interface.
+	 *
 	 * @param listener the listener which should be notified
-	 * 
-	 * @exception IllegalArgumentException <ul>
+	 *
+	 * @exception IllegalArgumentException
+	 *                <ul>
 	 *                <li>ERROR_NULL_ARGUMENT - if the listener is null</li>
 	 *                </ul>
-	 * @exception SWTException <ul>
+	 * @exception SWTException
+	 *                <ul>
 	 *                <li>ERROR_WIDGET_DISPOSED - if the receiver has been
 	 *                disposed</li>
 	 *                <li>ERROR_THREAD_INVALID_ACCESS - if not called from the
 	 *                thread that created the receiver</li>
 	 *                </ul>
-	 * 
+	 *
 	 * @see ModifyListener
 	 * @see #removeModifyListener
 	 */
 	public void addModifyListener(final ModifyListener listener) {
 		checkWidget();
-		this.panel.addModifyListener(listener);
+		panel.addModifyListener(listener);
 	}
 
 	/**
@@ -111,31 +116,33 @@ public class Calculator extends Composite {
 	 */
 	public String getValue() {
 		checkWidget();
-		return this.displayArea.getText();
+		return displayArea.getText();
 	}
 
 	/**
-	 * Removes the listener from the collection of listeners who will be
-	 * notified when the receiver's text is modified.
-	 * 
+	 * Removes the listener from the collection of listeners who will be notified
+	 * when the receiver's text is modified.
+	 *
 	 * @param listener the listener which should no longer be notified
-	 * 
-	 * @exception IllegalArgumentException <ul>
+	 *
+	 * @exception IllegalArgumentException
+	 *                <ul>
 	 *                <li>ERROR_NULL_ARGUMENT - if the listener is null</li>
 	 *                </ul>
-	 * @exception SWTException <ul>
+	 * @exception SWTException
+	 *                <ul>
 	 *                <li>ERROR_WIDGET_DISPOSED - if the receiver has been
 	 *                disposed</li>
 	 *                <li>ERROR_THREAD_INVALID_ACCESS - if not called from the
 	 *                thread that created the receiver</li>
 	 *                </ul>
-	 * 
+	 *
 	 * @see ModifyListener
 	 * @see #addModifyListener
 	 */
 	public void removeModifyListener(final ModifyListener listener) {
 		checkWidget();
-		this.panel.removeModifyListener(listener);
+		panel.removeModifyListener(listener);
 	}
 
 	/**
@@ -146,7 +153,7 @@ public class Calculator extends Composite {
 	public void setValue(final String value) {
 		checkWidget();
 		new Double(value);
-		this.displayArea.setText(value);
+		displayArea.setText(value);
 	}
 
 }
